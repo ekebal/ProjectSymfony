e97f94b54338a94dbd10ee6264d844dd70e1412e
ikbal hamdaoui <ikbal2625@gmail.com>
2016-12-16 20:18:14 +0100

    Blog

---
+++ blog_ikbal/src/IkbalBlogBundle/Form/ArticleType.php
@@ -0,0 +1,24 @@
+<?php
+namespace IkbalBlogBundle\Form;
+
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->setMethod('POST')
+         ->add('title')
+         ->add('author')
+         ->add('picture')
+         ->add('content', CKEditorType::class)
+         ->add('save', SubmitType::class);
+    }
+}
+
+
+?>
